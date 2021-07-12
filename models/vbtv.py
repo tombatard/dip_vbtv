2fb3afe60536e783e2b8b0fa8a902a3dd6b2c914
tombatard <87153726+tombatard@users.noreply.github.com>
2021-07-12 21:44:32 +0200

    Add files via upload

--- models/vbtv.py
+++ models/vbtv.py
@@ -10,6 +10,23 @@ torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark =True
 dtype = torch.cuda.FloatTensor
 
+class DIP(nn.Module):
+
+	def __init__(self,input_depth, pad, upsample_mode, n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
+		super(DIP,self).__init__()
+		self.net = skip(input_depth, n_channels, num_channels_down = [skip_n33d]*num_scales if isinstance(skip_n33d, int) else skip_n33d,
+                                            num_channels_up =   [skip_n33u]*num_scales if isinstance(skip_n33u, int) else skip_n33u,
+                                            num_channels_skip = [skip_n11]*num_scales if isinstance(skip_n11, int) else skip_n11, 
+                                            upsample_mode=upsample_mode, downsample_mode=downsample_mode,
+                                            need_sigmoid=True, need_bias=True, pad=pad, act_fun=act_fun)
+
+		self.input_depth=input_depth
+
+	def forward(self, input):
+
+		return self.net(input)
+
+
 class VectorialTotalVariation(nn.Module):	
 
 	def __init__(self,input_depth, pad, height, width, upsample_mode, n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
